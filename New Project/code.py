5a4fe1ea4ed75697b149db9fafb0beb6667e74b7
itzipper <ajisafelukmanm@gmail.com>
2023-02-27 00:30:54 +0100

    all file

---
+++ New Project/code.py
@@ -0,0 +1,15 @@
+ <section id="Gallery" class="main-selector">Gallery Page</section>
+        <section id="Home" class="main-selector">Home Page</section>
+        <section id="Contact" class="main-selector">Contact Page</section>
+        <section id="Location" class="main-selector">Location Page</section>
+        <section id="Address" class="main-selector">Address Page</section>
+    </div>
+    <header class="lukman">
+        <nav class="Container">
+            <a href="#Gallery">Gallery</a>
+            <a href="#Home">Home</a>
+            <a href="#Contact">Contact</a>
+            <a href="#Location">Location</a>
+            <a href="#Address">Address</a>
+        </nav>
+        
